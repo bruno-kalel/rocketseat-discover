0f42cc5d27db7d4d6518caa6bee5ad98f87b4a4d
Bruno Kalel <104171372+bruno-kalel@users.noreply.github.com>
2023-09-20 09:21:08 -0300

    20 set 2

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-function toggleMode() {
+function toggleMode()
+{
   const html = document.documentElement
   html.classList.toggle("light")
-
   const img = document.querySelector("#profile img")
-
-  // if (html.classList.contains("light"))
-  // {
-  //   img.setAttribute("src", "https://www.github.com/bruno-kalel")
-  // } else
-  // {
-  //   img.setAttribute("src", "https://www.github.com/bruno-kalel")
-  // }
 }
